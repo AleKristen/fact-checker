3e4edd3db912f0413ac48576afb9b4232c2475bb
AleKristen <correo@gmail.com>
2020-09-01 19:13:54 -0300

    Questions

--- src/app/components/level1/level1.component.ts
+++ src/app/components/level1/level1.component.ts
@@ -7,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Level1Component implements OnInit {
 
+  question: Object = {};
+  questions: Array<{}> = [
+    {title: "Las personas con asma tienen mayor riesgo ante el COVID-19",
+    options: [{a: "verdadero", b:"falso"}]},
+    {title: "Las infusiones con miel, limón o jengibre ¿curan el COVID-19?",
+    options: [{a: "falso", b:"verdadero"}]},
+    {title: "¿Las moscas domésticas pueden transmitir el COVID-19?",
+    options: [{a: "verdero", b:"falso"}]},
+    {title: "¿El coronavirus puede afectar a personas jóvenes?",
+    options: [{a: "verdero", b:"falso"}]},
+    {title: "Los termómetros sin contacto NO detectan el COVID-19",
+    options: [{a: "verdero", b:"falso"}]},
+    {title: "¿Rociar el cuerpo con alcohol y cloro es efectivo para eliminar el Coronavirus?",
+    options: [{a: "verdero", b:"falso"}]},
+  ];
+  index:any;
+  history = [];
   constructor() { }
-
-  ngOnInit(): void {
-    console.log('holi')
+  setHistory(index) {
+    this.history.push(index);
+  }
+  checkHistory(index) {
+    return this.history.includes(index);
+  }
+  getRandomIndex() {
+    return Math.floor(Math.random() * this.questions.length);
+  }
+  nextQuestion(){
+    this.index = this.getRandomIndex();
+    if(this.checkHistory(this.index) === false) {
+      this.question = this.questions[this.index];
+    } else {
+      this.index = this.getRandomIndex();
+    }
+    this.setHistory(this.index);
   }
 
+  ngOnInit() {
+
+  }
 }
+
+
